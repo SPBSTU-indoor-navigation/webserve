1fcb0960477742deaedc932aceb8b399fdb3b248
Andrei <soprachev@mail.ru>
2022-04-18 23:48:03 +0300

    aasa

--- app/routes/index.js
+++ app/routes/index.js
@@ -18,6 +18,15 @@ router.get('/', (req, res) => {
 router.get('/.well-known/apple-app-site-association', (req, res) => {
     res.json(
         {
+            applinks: {
+                apps: [],
+                details: [
+                    {
+                        appID: 'LZP5UN7552.com.soprachev.polymap',
+                        paths: ['*']
+                    }
+                ]
+            },
             appclips: {
                 apps: ["LZP5UN7552.com.soprachev.polymap.clip"]
             }
